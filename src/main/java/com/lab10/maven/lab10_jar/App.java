f71daaeafb7462d5ab15bdb1665c9053dff87bd3
Anishashettty <shettyanisha2004@gmail.com>
2025-05-28 21:09:47 +0530

    add

---
+++ src/main/java/com/lab10/maven/lab10_jar/App.java
@@ -0,0 +1,13 @@
+package com.lab10.maven.lab10_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
